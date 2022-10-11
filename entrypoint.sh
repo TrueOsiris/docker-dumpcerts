14e47055cd04171319af02da822824404c6a6203
trueosiris <tim@chaubet.be>
2022-10-11 15:52:35 +0200

    cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 echo "Starting cron container"
 echo "Adding environment variables before launching cron"
 printenv | grep -v "no_proxy" >> /etc/environment
-echo "Running cron with '$CRONSCHEDULE'=$CRONSCHEDULE and '$PROVIDER'=$PROVIDER"
+echo "Running cron with CRONSCHEDULE=$CRONSCHEDULE and PROVIDER=$PROVIDER"
 echo "$CRONSCHEDULE /dumpcerts $PROVIDER /acme/acme.json /acme" > /etc/crontabs/root 2>&1
 echo "# gg" >> /etc/crontabs/root 2>&1
-cron -f
+crond -l 2 -f
 
